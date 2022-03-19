b8240f0951d90ada128c9e6163fad93e26c7f4e4
srshin2020 <sangrimshin2020@gmail.com>
2022-03-19 14:58:10 +0900

    jsdoc

---
+++ jsdoc/src/index.js
@@ -0,0 +1,19 @@
+// @ts-check
+/**
+ * Student Name
+ * 학생 이름을 저장한다
+ * @type {string}
+ */
+const studentName = 'Amy';
+
+/**
+ * 점수 배열
+ * @type {Array<number>}
+ */
+const arr = [1, 2, 3, 4, 5];
+
+/**
+ * 할일
+ * @type {{id: number|string, text :string}}
+ */
+const todo = { id: 1, text: 'go market' }
